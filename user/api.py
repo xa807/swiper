0f85633413db2308f98ece6a881198d7aff78cc8
disen <610039018@qq.com>
2019-05-23 16:48:02 +0800

    Ajax文件上传和OSS文件操作

--- user/api.py
+++ user/api.py
@@ -1,8 +1,15 @@
 from __future__ import absolute_import
 
+import os
+import uuid
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
 from common.http import render_json
 from libs import sms
-
+from libs import oss
+from swiper import settings
+from user.models import User
 
 def get_code(request):
     if request.method == 'GET':
@@ -21,5 +28,36 @@ def get_code(request):
 
 def upload_avatar(request):
     # 上传用户头像
-    pass
+    if request.method == 'POST':
+        avatar_file: InMemoryUploadedFile = request.FILES.get('avatar')  # 要求：前端上传头像的字段为avatar
 
+        # 将上传的图片文件写入到临时文件中
+        filedir = os.path.join(settings.BASE_DIR, 'static/images')
+        filename = uuid.uuid4().hex + os.path.splitext(avatar_file.name)[-1]
+        with open(os.path.join(filedir,filename), 'wb') as f:
+            for chunk in avatar_file.chunks():
+                f.write(chunk)
+        return render_json(200,  {'path': '/static/images/%s' % filename})
+
+    return render_json(300,  '只允许POST请求')
+
+
+def update_avatar(request):
+    if request.method == 'GET':
+        filename = request.GET.get('filename')
+        key = oss.upload_file(settings.BASE_DIR+filename)
+
+        login_user = request.session['login_user']
+        login_user['avatar'] = oss.get_small_url(key)
+
+        user = User.objects.get(pk=login_user.get('uid'))
+        user.avatar = key
+        user.save()  # 更新头像的oss的key
+
+        # 删除临时文件
+        os.remove(settings.BASE_DIR+filename)
+
+        return render_json(200, {'url': login_user['avatar'],
+                                 'msg': '更新头像成功'})
+
+    return render_json(100,  '只允许GET请求')
--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from user import views
-from user.api import get_code
+from user import api
 
 urlpatterns = [
     path('regist/', views.regist),
-    path('code/', get_code),
+    path('code/', api.get_code),
+    path('upload_avatar/', api.upload_avatar),
+    path('update_avatar/', api.update_avatar),
     path('index/', views.to_index),
     path('login/',views.login),
     path('logout/',views.logout),
